959739707d59fd05bc271ff4e23570a8da404b89
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-02-06 13:57:06 +0800

    design weather result

--- src/App.js
+++ src/App.js
@@ -40,13 +40,20 @@ function App() {
             />
             {cityDetails && (
               <>
-                <ul>
-                  <li>Country: {cityDetails.sys.country}</li>
-                  <li>Name: {cityDetails.name}</li>
-                  <li>Temperature: {cityDetails.main.temp}</li>
-                  <li>Weather: {cityDetails.weather[0].description}</li>
-                </ul>
-                <div>{<DateBuilder newDate={new Date()} />}</div>
+                <div className="location-box">
+                  <div className="location">
+                    {cityDetails.name}, {cityDetails.sys.country}
+                  </div>
+                  <div className="date">
+                    {<DateBuilder newDate={new Date()} />}
+                  </div>
+                </div>
+                <div className="weather-box">
+                  <div className="temp">{cityDetails.main.temp} °C</div>
+                  <div className="weather">
+                    {cityDetails.weather[0].description}
+                  </div>
+                </div>
               </>
             )}
           </Cards>
